93ce5b7c55431185c03193cee6e1c9afdef8b8a3
Lalo Flores Cruz <eflores@crudcreativo.com>
2017-12-18 09:37:24 -0400

    Intent created on Main activity

--- app/src/main/java/com/crudcreativo/mird/MainActivity.java
+++ app/src/main/java/com/crudcreativo/mird/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main_activity);
 
 
@@ -21,6 +22,49 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent cibaoIntent = new Intent(MainActivity.this, CibaoActivity.class);
                 startActivity(cibaoIntent);
+            }
+        });
+
+        TextView este = (TextView) findViewById(R.id.tvEste);
+        este.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the colors category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent esteIntent = new Intent(MainActivity.this,  EsteActivity.class);
+                startActivity(esteIntent);
+            }
+        });
+
+
+        TextView mapa = (TextView) findViewById(R.id.tvMapa);
+        mapa.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the colors category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent mapaIntent = new Intent(MainActivity.this,  MapaActivity.class);
+                startActivity(mapaIntent);
+            }
+        });
+
+
+        TextView sd = (TextView) findViewById(R.id.tvSD);
+        sd.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the colors category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent sdIntent = new Intent(MainActivity.this,  SDActivity.class);
+                startActivity(sdIntent);
+            }
+        });
+
+
+        TextView sur = (TextView) findViewById(R.id.tvSD);
+        sur.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the colors category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent surIntent = new Intent(MainActivity.this,  SurActivity.class);
+                startActivity(surIntent);
             }
         });
 
