1da288827d056dce3a5204b8f54318a5ee6fa24a
Lalo Flores Cruz <eflores@crudcreativo.com>
2018-01-10 22:09:18 -0400

    Sur Adapter...

--- app/src/main/java/com/crudcreativo/mird/SurActivity.java
+++ app/src/main/java/com/crudcreativo/mird/SurActivity.java
@@ -3,6 +3,7 @@ package com.crudcreativo.mird;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,26 +16,20 @@ public class SurActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sur);
 
 
-        ArrayList<String> sur = new ArrayList<String>();
+        ArrayList<Sur> sur = new ArrayList<Sur>();
+        sur.add(new Sur("Cuevas del Pomier"));
+        sur.add(new Sur("Polo de Barahona"));
+        sur.add(new Sur("Dunas de Bani"));
+        sur.add(new Sur("Laguna de Oviedo"));
+        sur.add(new Sur("Bahia de Las Aguilas"));
+        sur.add(new Sur("Lago Enriquillo"));
+        sur.add(new Sur("Balneario La Zurza"));
+        sur.add(new Sur("Hoyo de Pelempito"));
+        sur.add(new Sur("Isla Cabritos"));
+        sur.add(new Sur("Bahia de Ocoa"));
 
-        sur.add("Cuevas del Pomier");
-        sur.add("Polo de Barahona");
-        sur.add("Dunas de Bani");
-        sur.add("Laguna de Oviedo");
-        sur.add("Bahia de Las Aguilas");
-        sur.add("Lago Enriquillo");
-        sur.add("Balneario La Zurza");
-        sur.add("Hoyo de Pelempito");
-        sur.add("Isla Cabritos");
-        sur.add("Bahia de Ocoa");
-
-
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-
-        for (int index = 0; index < sur.size(); index++) {
-            TextView surView = new TextView(this);
-            surView.setText(sur.get(index));
-            rootView.addView(surView);
-        }
+        SurAdapter itemAdapter = new SurAdapter(this, sur);
+        ListView listView = (ListView) findViewById(R.id.listview_sur);
+        listView.setAdapter(itemAdapter);
     }
 }
---
+++ app/src/main/java/com/crudcreativo/mird/SurAdapter.java
@@ -0,0 +1,39 @@
+package com.crudcreativo.mird;
+
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Eduardo Flores on 8/1/2018.
+ */
+
+public class SurAdapter extends ArrayAdapter<Sur> {
+
+    public SurAdapter(Activity context, ArrayList<Sur> surs) {
+        super(context, 0, surs);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item_sur, parent, false);
+        }
+
+        Sur currentSur= getItem(position);
+
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.site_name);
+        nameTextView.setText(currentSur.getSite());
+
+        return listItemView;
+    }
+
+}
